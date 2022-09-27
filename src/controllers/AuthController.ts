66b0e5fa65bfacd116ff5195a82bbb3bc3321653
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2022-09-27 13:25:00 -0300

    Correção authController

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -10,20 +10,21 @@ export default {
 
         try {
             if (!user) {
-                return res.json({ error: 'Usuário não encontrado.' });
+                return res.status(404).json({error: 'Usuário não encontrado.'})
             }
 
             const isEmailMatch = await email === user.email;
             const isPasswordMatch = await compare(password, user.password);
 
             if (!isEmailMatch || !isPasswordMatch) {
-                return res.json({ error: 'E-mail ou senha inválidos' });
+                return res.status(400).json({ error: 'E-mail ou senha inválidos.' });
             }
 
             const secret = String(process.env.SECRET);
             const token = sign({ id: user.id }, secret, { expiresIn: '1d' });
 
             return res.json({ user, token });
+
         } catch (error) {
             res.json({ error });
         }
